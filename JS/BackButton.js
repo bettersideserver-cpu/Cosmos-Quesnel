6d4cd6286ed11b8a643f58f9628544c7c0e590f1
betterside <bettersideserver@gmail.com>
2026-07-06 17:53:32 +0530

    f

--- JS/BackButton.js
+++ JS/BackButton.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (closeCard) {
         closeCard.addEventListener("click", () => {
           // 🔹 Change this path to wherever you want the button to take you
-          window.location.href = "/index.html";
+          window.location.href = "../index.html";
           // Example: "../index.html" or "../Subpages/Tower_A.html"
         });
       }
